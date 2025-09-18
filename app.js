8d7ef0e8f89ade819022ed0478945b250cbf0ecf
Adiitoh <davehallson58@gmail.com>
2025-09-18 03:41:54 +0100

    Add files via upload
    Working with EJS templates

--- app.js
+++ app.js
@@ -1,54 +1,80 @@
+const { createClient } =  require ('@supabase/supabase-js')
 const express = require("express")
-const axios = require("axios")
-var qs = require('qs')
+const bodyparser = require("body-parser")
 const app = express()
-const port = 3000
 
-app.get("/", async (req, res) => {
-    var cityName = "Limbe"
-    var apiKey = "646753a1fce30c7f34f6a6fff605ec5a"
-    var result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
-    var main = result.data.weather[0].main
-    var description = result.data.weather[0].description
-    console.log(main)
-    res.send(`the weather today is ${main} and the description is ${description}`)
+app.use(bodyparser.urlencoded())
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
 
 
-    //done getting the weather
-    //sending the weather as a whatsApp message
-    
-    var token = "dnq6xy7px5kwiue5"
-    var data = qs.stringify({
-        "token": token,
-        "to": "+237675920113",
-        "body": `Hello Cash the weather today is ${description}`
-    });
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://pibvwljdhclnzopeznsz.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpYnZ3bGpkaGNsbnpvcGV6bnN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ0OTA1NzIsImV4cCI6MjA2MDA2NjU3Mn0.DfKWUb4e37xeoG3NOnOot3D2NWE4mtx0xZvzy6xEYiA')
 
-    var INSTANCE_ID = "instance113969"
-    var config = {
-        method: 'post',
-        url: `https://api.ultramsg.com/${INSTANCE_ID}/messages/chat`,
-        headers: {
-            'Content-type': 'application/x-www-form-urlencoded'
-        },
-        data: data
-    };
+var fruits = ''
+var arrayOfFruits = []
+var arrayOfIDs = []
+app.get("/", async (req, res) =>{
+    var date = new Date()
+    var day = date.toDateString()
+    var {data} = await supabase.from("Fruits").select().not('id', 'in', `(${arrayOfIDs.join(',')})`)
 
-    axios(config)
-        .then(function (response) {
-            try{
-                console.log(JSON.stringify(response.data));
-            }catch(error){
-                console.log(error)
+    for (var i = 0; i < data.length; i++) {
+        arrayOfFruits.push(data[i].name)
+        arrayOfIDs.push(data[i].id)
+    }
+
+    res.render("home", {date : day, newFruits: arrayOfFruits})
+})
+
+app.get("/try", (req, res) => {
+    res.render("try")
+})
+
+app.post("/", async (req, res) => {
+    fruits = req.body.newFruit;
+    var button = req.body.button;
+    var newValue = req.body.newValue;
+    var oldValue = req.body.oldValue;
+    var deletedItem = req.body.delete;
+
+    if (button == 'insert'){
+        // write operation
+        await supabase
+            .from('Fruits')
+            .insert({ name: fruits })
+    } else if(button == 'update'){
+        //update operation
+        const {data} = await supabase
+            .from('Fruits')
+            .update({name: newValue})
+            .eq('name', oldValue)
+        for (var i=0; i < arrayOfFruits.length; i++){
+            if (arrayOfFruits[i] == oldValue){
+                 arrayOfIDs.pop(arrayOfIDs[i])
+                 arrayOfFruits.pop(arrayOfFruits[i])
             }
-        })
-        .catch(function (error) {
-            console.log(error);
+        }
+    } else {
+        // delete querry 
+        await supabase
+            .from('Fruits')
+            .delete()
+            .eq('name', deletedItem)
+        for (var i = 0; i < arrayOfFruits.length; i++){
+             if (arrayOfFruits[i] == deletedItem){
+                 arrayOfFruits.pop(arrayOfFruits[i])
+                 arrayOfIDs.pop(arrayOfIDs[i])
+             }
+        }
 
-        });
-            
-    })
-app.listen(port, (req, res) => {
-    console.log("Server is running on port 3000")
 
+    }           
+
+    res.redirect("/")
+})
+
+
+app.listen(3000, (req, res) =>{
+    console.log("server is running on port 3000")
 })
